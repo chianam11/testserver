2e71004dbdc3f72406ac3704a0627ad40de7c5fe
chianam11 <chinam31x@gmail.com>
2024-08-09 17:23:56 +0700

    update

--- api/app.js
+++ api/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.json({mes:"okokok"}));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(4000, () => console.log("Server ready on port 4000."));
 
 module.exports = app;
