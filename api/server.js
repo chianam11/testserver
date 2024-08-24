5e191db12e528846704c6bc001c25cba5e7621fb
chinam <chinam31x@gmail.com>
2024-08-25 02:31:49 +0700

    upd

--- api/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => res.json({mes:"okokok"}));
-
-app.listen(4000, () => console.log("Server ready on port 4000."));
-
-module.exports = app;
